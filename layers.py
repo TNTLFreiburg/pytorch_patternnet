e111d76827bb998c57553a162bd7fdd866af6eee
rturon <rturon@uos.de>
2019-05-27 10:28:18 +0200

    PatternNetLayers untested

--- layers.py
+++ layers.py
@@ -1,10 +1,12 @@
 import torch
 import numpy as np
 import torch.nn as nn
+import patterns
 
 class PatternConv2d(nn.Module):
 
     def __init__(self, conv_layer):
+        super(PatternConv2d, self).__init__()
 
         self.forward_layer = conv_layer  # kernels size of forward layer: 
                                          # self.forward_layer.kernel_size
@@ -28,7 +30,7 @@ class PatternConv2d(nn.Module):
             output, but also output without bias, if the forward layer has a 
             bias parameter.
         '''
-
+        
         def expand_bias(bias, size):
             new_tensor = torch.zeros((size))
             for i in range(bias.shape[0]):
@@ -36,7 +38,9 @@ class PatternConv2d(nn.Module):
 
             return new_tensor
 
+        
         output = self.forward_layer(input)
+        # TODO: what if the layer does not have a bias?
         bias = expand_bias(self.forward_layer.bias.data)
         output_wo_bias = output - bias
 
@@ -103,7 +107,7 @@ class PatternConv2d(nn.Module):
         self.patterns = patterns.compute_patterns_conv(self.statistics, 
                                                        kernel)
 
-    def set_patterns(pattern_type='relu'):
+    def set_patterns(self, pattern_type='relu'):
         ''' Sets the computed patterns as the kernel of the backward layer.
             pattern_type can be 'relu' or 'linear'
         '''
@@ -112,3 +116,146 @@ class PatternConv2d(nn.Module):
         elif pattern_type == 'linear':
             next(self.backward_layer.parameters()) = self.patterns['A_linear']
 
+
+class PatternLinear(nn.Module):
+
+    def __init__(self, linear_layer):
+        super(PatternLinear, self).__init__()
+
+        self.forward_layer = linear_layer 
+
+        self.backward_layer = nn.Linear(linear_layer.out_features, 
+                                        linear_layer.in_features, 
+                                        bias=False)
+
+        self.statistics = None
+        self.patterns = None
+
+    def forward(self, input):
+        ''' perform forward computations of forward_layer, return not only new
+            output, but also output without bias, if the forward layer has a 
+            bias parameter.
+        '''
+
+        def expand_bias(bias, size):
+            new_tensor = torch.zeros((size))
+            for i in range(bias.shape[0]):
+                new_tensor[:, i] = bias[i]
+
+            return new_tensor
+
+        output = self.forward_layer(input)
+        # TODO: what if the layer does not have a bias?
+        bias = expand_bias(self.forward_layer.bias.data)
+        output_wo_bias = output - bias
+
+        return output, output_wo_bias
+
+
+    def backward(self, input):
+        ''' compute a backward step (for signal computation).
+        '''
+        output = self.backward_layer(input)
+        # rescale output to be between -1 and 1
+        absmax = torch.abs(output.data).max()
+        if absmax > 0.000001:
+            output.data /= absmax
+        output.data[output.data > 1] = 1
+        output.data[output.data < -1] = -1
+
+        return output
+
+
+    def compute_statistics(self, input, output, output_wo_bias=None):
+        ''' compute statistics for this layer given the input, output and 
+            output without bias. Initialize statistics if none there yet,
+            otherwise update statistics with new values.
+
+            If the forward layer does not use a bias term, then the output
+            without bias, i.e. the layer's output, is in output and there is 
+            no tensor in output_wo_bias.
+        '''
+        if output_wo_bias is None:
+            if self.statistics is None:
+                self.statistics = patterns.compute_statistics(input, 
+                                                              output, 
+                                                              output)
+            else:
+                self.statistics = patterns.update_statistics(input,
+                                                             output,
+                                                             output,
+                                                             self.statistics)
+
+        else:
+            if self.statistics is None:
+                self.statistics = patterns.compute_statistics(input, 
+                                                            output_wo_bias, 
+                                                            output)
+            else:
+                self.statistics = patterns.update_statistics(input,
+                                                             output_wo_bias,
+                                                             output,
+                                                             self.statistics)
+        
+
+    def compute_patterns(self):
+        ''' Compute patterns from the computed statistics. 
+        '''
+        w = next(self.forward_layer.parameters())
+        self.patterns = patterns.compute_patterns_linear(self.statistics, w)
+
+
+    def set_patterns(self, pattern_type='relu'):
+        ''' Sets the computed patterns as the kernel of the backward layer.
+            pattern_type can be 'relu' or 'linear'
+        '''
+        if pattern_type == 'relu':
+            next(self.backward_layer.parameters()) = self.patterns['A_plus']
+        elif pattern_type == 'linear':
+            next(self.backward_layer.parameters()) = self.patterns['A_linear']
+
+
+class PatternReLU(nn.Module):
+
+    def __init__(self):
+        super(PatternReLU, self).__init__()
+
+        self.forward_layer = nn.ReLU()
+
+    def forward(self, input):
+        indices = input <= 0
+        output = self.forward_layer(input)
+
+        return output, indices
+
+    def backward(self, input, indices):
+        # copy the input
+        input = torch.new_tensor(input)
+        input[indices] = 0
+
+        return input
+
+
+class PatternMaxPool2d(nn.Module):
+
+    def __init__(self, pool_layer):
+        super(PatternMaxPool2d, self).__init__()
+
+        # create a new pooling layer to use a different instance with 
+        # return_indices=True without changing the original layer's
+        # settings
+        self.forward_layer = nn.MaxPool2d(pool_layer.kernel_size,
+                                          pool_layer.stride,
+                                          pool_layer.padding,
+                                          pool_layer.dilation,
+                                          return_indices=True)
+        self.backward_layer = nn.MaxUnpool2d(pool_layer.kernel_size, 
+                                             pool_layer.stride, 
+                                             pool_layer.padding)
+
+    def forward(self, input):
+        return self.forward_layer(input)
+
+    def backward(self, input, switches):
+        return self.backward_layer(input, switches)
+    
--- networks.py
+++ networks.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import patterns
 import time
+import layers
 
 
 class MnistNet(nn.Module):
@@ -404,3 +405,205 @@ class BackwardReLU(torch.nn.Module):
         inp[indices] = 0
 
         return inp
+
+
+class PatternNetLayers(torch.nn.Module):
+
+    def __init__(self, layers_lst):
+        super(PatternNetLayers, self).__init__()
+
+        # initialize variables for later use
+        self.lst_layers = layers_lst
+        # save change from conv to linear layers to know when to reshape
+        # get first conv or linear layer
+        cur_layer_ind = 0
+        while not (self.lst_layers[cur_layer_ind].__class__.__name__ in \
+                ["Conv2d","Linear"]) and cur_layer_ind < len(self.lst_layers):
+
+            cur_layer_ind+=1
+        # if only the last layer is conv or linear, do not need to find next layer
+        if cur_layer_ind < len(self.lst_layers) -1:
+            cur_layer = self.lst_layers[cur_layer_ind].__class__.__name__
+            next_layer_ind = cur_layer_ind + 1
+            while cur_layer_ind < len(self.lst_layers) -1:
+                while not (self.lst_layers[next_layer_ind].__class__.__name__ in \
+                        ["Conv2d","Linear"]) and next_layer_ind < len(self.lst_layers):
+                    next_layer_ind+=1
+                next_layer = self.lst_layers[next_layer_ind].__class__.__name__
+                # now check if there's a change between conv and linear layers
+                if cur_layer == "Conv2d" and next_layer == "Linear":
+                    self.reshape_ind = next_layer_ind
+                cur_layer_ind = next_layer_ind
+                cur_layer = next_layer
+                next_layer_ind = cur_layer_ind +1
+
+
+        # initialize the backward layers
+        self.backward_layers = []
+        for layer in self.lst_layers[::-1]:
+            if layer.__class__.__name__ == "Conv2d":
+                self.backward_layers.append(layers.PatternConv2d(layer))
+            if layer.__class__.__name__ == "Linear":
+                self.backward_layers.append(layers.PatternLinear(layer))
+            if layer.__class__.__name__ == "MaxPool2d":
+                self.backward_layers.append(layers.PatternMaxPool2d(layer))
+            if layer.__class__.__name__ == "ReLU":
+                self.backward_layers.append(layers.PatternReLU)
+
+    def compute_signal(self, img, only_biggest_value=True):
+        """ Additional method to compute the signal for a given image. """
+        return self.forward(img, only_biggest_value)
+
+    def forward(self, img, only_biggest_value=True):
+
+        output, _, indices, switches = self.forward_with_extras(img)
+
+        if not only_biggest_value:
+            y = output
+        # use only highest valued output
+        else:
+            # atm only works for single images, make it work with several 
+            # images as well
+            y = torch.zeros(output.size(), requires_grad=False)
+            max_v, max_i = torch.max(output.data, dim=1)
+            # max_i = int(max_i.numpy())
+            # max_v = float(max_v.numpy())
+            y[range(y.shape[0]), max_i] = max_v
+
+        ind_cnt = 0
+        switch_cnt = 0
+
+        # go through all layers and apply their backward pass functionality
+        for ind, layer in enumerate(self.backward_layers):
+            if layer.__class__.__name__ == "BackwardReLU":
+                mask = indices[ind_cnt]
+                y = layer(y, mask)
+                ind_cnt += 1
+            elif layer.__class__.__name__ == "MaxUnpool2d":
+                y = layer(y, switches[switch_cnt])
+                switch_cnt += 1
+            else:
+                # if other layer than linear or conv, could theoretically
+                # be applied here without noticing
+                y = layer(y)
+                # scale output to be between -1 and 1
+                absmax = torch.abs(y.data).max()
+                if absmax > 0.000001:
+                    y.data = y.data / absmax
+                y.data[y.data > 1] = 1
+                y.data[y.data < -1] = -1
+
+                # check if reshape is necessary
+                if len(self.lst_layers) - ind == self.reshape_ind + 1:
+                    s = self._reshape_size_in
+                    y.data = y.data.view(-1, s[1], s[2], s[3])
+ 
+        return y
+
+    def forward_with_extras(self, imgs):
+        """
+        Performs one forward pass through the network given at initialization
+        (only convolutional, linear, pooling and ReLU layer). Additionally to
+        the final output the input and output to each convolutional and linear
+        layer, the switches of pooling layers and the indices of the values
+        that are set to zero of each ReLU layer, are returned.
+        """
+
+        output = Variable(imgs, requires_grad=False)
+
+        layers = []
+        layers_wo_bias = []
+        cnt = 0
+        indices = []
+        switches = []
+
+        # def expand_bias(bias, size):
+        #     new_tensor = torch.zeros((size))
+        #     for i in range(bias.shape[0]):
+        #         if len(size) == 4:
+        #             new_tensor[:, i, :, :] = bias[i]
+        #         else:
+        #             new_tensor[:, i] = bias[i]
+
+        #     return new_tensor
+
+        for ind, layer in enumerate(self.backward_layers[::-1]):
+            if layer.__class__.__name__ == "PatternConv2d":
+                # save input to layer
+                layers.append({})
+                layers[ind]["inputs"] = output.data
+                # apply forward layer
+                output, output_wo_bias = layer(output)
+                # save output of layer
+                layers[ind]["outputs"] = output.data
+                # save output without bias
+                layers_wo_bias.append(output_wo_bias)
+                cnt += 1
+            elif layer.__class__.__name__ == "PatternLinear":
+                # save input to layer
+                layers.append({})
+                layers[ind]["inputs"] = output.data
+                # apply layer
+                output, output_wo_bias = layer(output)
+                # save output of layer
+                layers[ind]["outputs"] = output.data
+                # save output without bias
+                layers_wo_bias.append(output_wo_bias)
+                cnt += 1
+            elif layer.__class__.__name__ == "PatternMaxPool2d":
+                # set return indices to true to get the switches
+                # apply layer
+                output, switch = layer(output)
+                # save switches
+                switches.append(switch)
+            elif layer.__class__.__name__ == "PatternReLU":
+                # save indices smaller zero
+                output, inds = layer(output)
+                indices.append(inds)
+
+            # add view between convolutional and linear sequential
+            if ind == self.reshape_ind-1:  # layer before the first linear layer
+                    self._reshape_size_in = output.shape
+                    output = output.view(-1,self.lst_layers[ind+1].in_features)
+
+
+        return (
+            output,
+            (layers, layers_wo_bias),
+            indices[::-1],
+            switches[::-1],
+        )
+
+    def compute_statistics(self, imgs, use_bias=False):
+        """ Initializes statistics if no statistics were computed
+            before. Otherwise updates the already computed statistics.
+        """
+
+        # get the layer outputs
+        _, outputs, _, _ = self.forward_with_extras(imgs)
+        layer_outputs = outputs[0]
+        layer_outputs_wo_bias = outputs[1]
+
+        # cnt for layers with params
+        cnt = 0
+        for layer in self.backward_layers[::-1]:
+            if layer.__class__.__name__ in ["PatternConv2d", "PatternLinear"]:
+                layer.compute_statistics(layer_outputs[cnt]["inputs"],
+                                         layer_outputs[cnt]["outputs"],
+                                         layer_outputs_wo_bias[cnt])
+                cnt += 1
+
+
+    def compute_patterns(self):
+
+            for layer in self.backward_layers[::-1]:
+                if layer.__class__.__name__ in ["PatternConv2d", "PatternLinear"]:
+                    layer.compute_patterns()
+
+
+    def set_patterns(self, pattern_type="A_plus"):
+        """ pattern_type can either be A_plus or A_linear
+        """
+        for layer in self.backward_layers[::-1]:
+            if layer.__class__.__name__ in ["PatternConv2d", "PatternLinear"]:
+                layer.set_patterns(pattern_type=pattern_type)
